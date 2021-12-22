84998ea343e3e59fa83922dcb901e7851aca2548
Gajendra Saini <rickysaini2003@gmail.com>
2021-12-22 19:17:29 +0530

    Day1 on JS completed

---
+++ javascript/01.js
@@ -0,0 +1,116 @@
+// console.log("Hello World");
+// var firstname = "why this";
+// console.log(firstname);
+// var firstName = "unknown 32";
+// console.log(firstName);
+// var value1 = 2;
+// console.log(value1 * 4);
+// var new_func = 15;
+// console.log(new_func);
+// var $gajju = 55;
+// console.log($gajju);
+// var _nothing = 65;
+// console.log(_nothing);
+
+// we can make variable using var keyword
+// var sname = "why this";
+// sname = "why";
+// console.log(sname);
+
+// let fname = "nothing";
+// fname = "pppp";
+// console.log(fname);
+
+// const pi = 3.14;
+// console.log(pi);
+
+// let fname = "rahul";
+// console.log(fname[4]);
+// console.log(fname.length);
+// console.log(fname[fname.length - 1]);
+
+// let a = "sTrIng";
+// console.log(a);
+// let b = a.toUpperCase();
+// let c = a.toLowerCase();
+// console.log(c);
+// console.log(b);
+
+// let a = "Javascript";
+
+// let b = a.slice(3);
+// console.log(b);
+
+// let a = 55;
+// let b = "hello";
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof 7 );
+
+// let a = 22;
+// console.log(a);
+// a = a + "";
+// console.log(typeof a);
+
+// let b = 45;
+// b = String(b);
+// console.log(typeof b);
+
+// let c = "string";
+// c = Number(c);
+// console.log(typeof c);
+
+// let a = "12";
+// let b = "14";
+// let c = +a + +b; // changing to number using + sign
+// console.log(c);
+
+// template string
+
+// let a = "string";
+// let b = 10;
+// let c = `this is ${a} and is value ${b}`;
+// console.log(c);
+
+// console.log(typeof null); // bug in js
+
+// console.log(Number.MAX_SAFE_INTEGER);
+// let a = 12n;
+// let b = 34n;
+// console.log(a + b);
+
+// let a = "23";
+// let b = 23;
+// console.log(a > b);
+// console.log(a == b); // only take care of value not datatype
+// console.log(a === b);
+
+// If else conditions
+
+// let age = 55;
+// if (age < 18) {
+//   console.log("user can play games");
+// } else {
+//   console.log("this is running");
+// }
+
+// console.log(typeof (7 / 2));
+
+// falsy values
+// ""
+// false
+// 0
+// null
+// undefined
+
+// other then falsy value all values is true
+
+// ternery variable
+
+// let age = 5;
+// let drink = age < 2 ? "string" : "nothing";
+// console.log(drink);
+
+// let null undefined var const
+
+console.log("hello ");
